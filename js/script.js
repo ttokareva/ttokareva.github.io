5938b7983c7b5fcd62ac4e97fc32709634ee0342
ttokareva <tat.tokareva@gmail.com>
2025-03-20 23:46:11 -0700

    delete old files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav-links');
-    const toggleBtn = document.querySelector('.toggle-nav');
-
-    toggleBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-});
